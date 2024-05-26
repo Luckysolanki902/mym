56113195256809a9d3c3bbd7decc113d6bd0f140
Lucky <luckysolanki902@gmail.com>
2024-05-26 18:22:56 +0530

    fixing filters

--- components/chatComps/FilterOptions.js
+++ components/chatComps/FilterOptions.js
@@ -24,7 +24,7 @@ const FilterOptions = ({ userDetails }) => {
   });
   // filters contexts
   const { preferredGender, setPreferredGender, preferredCollege, setPreferredCollege } = useFilters();
-
+console.log({preferredCollege, preferredGender})
   const handleFilterToggle = () => {
     if (!userDetails) {
       return;
