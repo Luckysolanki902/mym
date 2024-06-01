eb0d07f0fcc355a524a2a82f7a27b35e7a9a0748
Lucky <luckysolanki902@gmail.com>
2024-06-01 18:56:34 +0530

    removed google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -18,18 +18,6 @@ export default class MyDocument extends Document {
               `
             }}
           />
-          {/* Google Tag Manager */}
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-MVLJQ1YYLG"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-MVLJQ1YYLG');
-              `
-            }}
-          />
           {/* Google Fonts */}
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Dancing+Script&display=swap"/>
           {/* Other head elements can be added here */}
