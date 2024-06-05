221e4f7c8da1761ae3f78a4605fb99768b67de05
Lucky <luckysolanki902@gmail.com>
2024-06-05 16:23:43 +0530

    added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,21 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-        {/* <link rel="icon" href="/images/mym_logos/mymlogoinvert.png" /> */}
+          {/* <link rel="icon" href="/images/mym_logos/mymlogoinvert.png" /> */}
           {/* Link to Google Fonts for Inter font */}
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Dancing+Script&display=swap"/>
+          {/* Google Analytics */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-MVLJQ1YYLG"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-MVLJQ1YYLG');
+              `,
+            }}
+          />
           {/* Other head elements can be added here */}
         </Head>
         <body>
