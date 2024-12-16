bec07c2563037f6f6e36f746147907bf29040b3d
Lucky <luckysolanki902@gmail.com>
2024-12-16 23:59:18 +0530

    still fixing drawer

--- components/appComps/Drawer.js
+++ components/appComps/Drawer.js
@@ -132,7 +132,7 @@ export default function SwipeableTemporaryDrawer() {
       (path) => path === currentPath
     );
     setActiveIndex(index !== -1 ? index : null);
-    console.info({currentPath, arrayPathValue: paths[index], index  })
+    console.info({ currentPath, arrayPathValue: paths[index], index })
   }, [router.pathname]);
 
   // Handle back button when drawer is open
@@ -153,9 +153,9 @@ export default function SwipeableTemporaryDrawer() {
     };
   }, [router]);
 
-  useEffect(()=>{
-    console.log({activeIndex})
-  },[activeIndex])
+  useEffect(() => {
+    console.log({ activeIndex })
+  }, [activeIndex])
 
   const list = () => (
     <div className={styles.MainCont} role="presentation">
@@ -195,73 +195,72 @@ export default function SwipeableTemporaryDrawer() {
             { text: 'Write Confession', href: '/create-confession' },
             { text: 'Inbox', href: '/inbox' },
             { text: 'Suggestions', href: '/give-your-suggestion' },
-          ].map((item, index) => 
-            {
-              console.log('compLog: ', index, activeIndex, activeIndex === index)
-              return(
-            <ListItem key={item.text} className={styles.sideBarListItem}>
-              <Link href={item.href} passHref style={{ width: '100%', textDecoration: 'none' }}>
-                <ListItemButton
-                  onClick={() => handleNavigation(item.href, index)}
-                  className={`${styles.sideBarLinks} ${
-                    activeIndex === index ? styles.activeListItem : ''
-                  }`}
-                >
-                  <ListItemIcon className={styles.listItemIcon}>
-                    {index === 0 ? (
-                      <Image
-                        src={'/images/sidebaricons/home.png'}
-                        width={170}
-                        height={170}
-                        alt="icon"
-                        className={`${styles.iconspng1} ${styles.sideIcon}`}
-                      />
-                    ) : index === 1 ? (
-                      <Image
-                        src={'/images/sidebaricons/randomchat.png'}
-                        width={108}
-                        height={72}
-                        alt="icon"
-                        className={`${styles.iconspng2} ${styles.sideIcon}`}
-                      />
-                    ) : index === 2 ? (
-                      <Image
-                        src={'/images/sidebaricons/confessions.png'}
-                        width={54.5}
-                        height={72}
-                        alt="icon"
-                        className={`${styles.iconspng3} ${styles.sideIcon}`}
-                      />
-                    ) : index === 3 ? (
-                      <Image
-                        src={'/images/sidebaricons/createconfession.png'}
-                        width={112.5}
-                        height={136}
-                        alt="icon"
-                        className={`${styles.iconspng4} ${styles.sideIcon}`}
-                      />
-                    ) : index === 4 ? (
-                      <StyledBadge badgeContent={unseenCount} color="primary">
-                        <MailIcon fontSize="medium" style={{ color: 'white' }} />
-                      </StyledBadge>
-                    ) : index === 5 ? (
-                      <Image
-                        src={'/images/sidebaricons/bulb.png'}
-                        width={150}
-                        height={136}
-                        alt="icon"
-                        className={`${styles.iconspng5} ${styles.sideIcon}`}
-                      />
-                    ) : null}
-                  </ListItemIcon>
-                  <ListItemText
-                    primary={<div className={styles.linkText}>{item.text}</div>}
-                    className={styles.link}
-                  />
-                </ListItemButton>
-              </Link>
-            </ListItem>
-          )})}
+          ].map((item, index) => {
+            return (
+              <ListItem key={item.text} className={styles.sideBarListItem}>
+                <Link href={item.href} passHref style={{ width: '100%', textDecoration: 'none' }}>
+                  <div
+                    style={{ backgroundColor: activeIndex == index ? 'rgba(211, 211, 211, 0.2)' : 'transparent' }}
+                    onClick={() => handleNavigation(item.href, index)}
+                    className={`${styles.sideBarLinks} 
+                  `}
+                  >
+                    <ListItemIcon className={styles.listItemIcon}>
+                      {index === 0 ? (
+                        <Image
+                          src={'/images/sidebaricons/home.png'}
+                          width={170}
+                          height={170}
+                          alt="icon"
+                          className={`${styles.iconspng1} ${styles.sideIcon}`}
+                        />
+                      ) : index === 1 ? (
+                        <Image
+                          src={'/images/sidebaricons/randomchat.png'}
+                          width={108}
+                          height={72}
+                          alt="icon"
+                          className={`${styles.iconspng2} ${styles.sideIcon}`}
+                        />
+                      ) : index === 2 ? (
+                        <Image
+                          src={'/images/sidebaricons/confessions.png'}
+                          width={54.5}
+                          height={72}
+                          alt="icon"
+                          className={`${styles.iconspng3} ${styles.sideIcon}`}
+                        />
+                      ) : index === 3 ? (
+                        <Image
+                          src={'/images/sidebaricons/createconfession.png'}
+                          width={112.5}
+                          height={136}
+                          alt="icon"
+                          className={`${styles.iconspng4} ${styles.sideIcon}`}
+                        />
+                      ) : index === 4 ? (
+                        <StyledBadge badgeContent={unseenCount} color="primary">
+                          <MailIcon fontSize="medium" style={{ color: 'white' }} />
+                        </StyledBadge>
+                      ) : index === 5 ? (
+                        <Image
+                          src={'/images/sidebaricons/bulb.png'}
+                          width={150}
+                          height={136}
+                          alt="icon"
+                          className={`${styles.iconspng5} ${styles.sideIcon}`}
+                        />
+                      ) : null}
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={<div className={styles.linkText}>{item.text}</div>}
+                      className={styles.link}
+                    />
+                  </div>
+                </Link>
+              </ListItem>
+            )
+          })}
         </div>
         {/* Bottom actions (e.g., Logout) */}
         {userDetails && (
