67443efe9aab747eb5c21370a105599d4a00aaea
Lucky Solanki <luckysolanki902@gmail.com>
2025-12-10 03:25:28 +0530

    improve: uninstall unused packages to reduce bundle size

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,13 @@
 const nextCoreWebVitals = require('eslint-config-next/core-web-vitals');
 
-module.exports = [...nextCoreWebVitals];
+module.exports = [
+  ...nextCoreWebVitals,
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      'react-hooks/exhaustive-deps': 'warn',
+      '@next/next/no-html-link-for-pages': 'warn',
+      '@next/next/next-script-for-ga': 'warn',
+    },
+  },
+];
